d433bebb527869ca106dae93e88e59de03223152
Stalbek <stalbekghingizov@gmail.com>
2026-02-22 13:27:39 +0600

    баштоого даяр

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,20 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import CommentForm from './componients/CommentForm'
+import CommentList from './componients/CommentList'
+import PostItem from './componients/PostItem'
+import PostList from './componients/PostList'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <CommentForm/>
+      <CommentList/>
+      <PostItem/>
+      <PostList/>
     </>
   )
 }
